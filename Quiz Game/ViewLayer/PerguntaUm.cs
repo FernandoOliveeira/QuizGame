7dccc8c7680884ce3069f1ea0065197a3a96d337
Fernando <fernando.silva789@etec.sp.gov.br>
2020-01-09 23:01:34 -0300

    GameOver, MainMenu, PerguntaUm: Removidos todos os using's desnecessários

--- Quiz Game/ViewLayer/PerguntaUm.cs
+++ Quiz Game/ViewLayer/PerguntaUm.cs
@@ -1,23 +1,11 @@
 ﻿using System;
-using System.Collections.Generic;
-using System.ComponentModel;
-using System.Data;
-using System.Drawing;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 using System.Windows.Forms;
-using System.Media;
-using System.IO;
 using Quiz_Game.Control;
-using System.Threading;
 
 namespace Quiz_Game.ViewLayer
 {
     public partial class PerguntaUm : Form
     {
-        Thread thread;
-
         public PerguntaUm()
         {
             InitializeComponent();
